5ef8301e00f3fc79d7f2b3d575282c7f404ba2e7
Sumit Chalotra <kumar.sumit9981@gmail.com>
2021-03-28 17:18:03 +0530

    updated sg key and removed bodyParser

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ mongoose.connect(
   console.log('Connection Failed!');
 });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use("/images",express.static(path.join(__dirname,'images')));
 
 app.use((req, res, next) => {
